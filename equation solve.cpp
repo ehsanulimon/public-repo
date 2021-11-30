b904f49cbf47ad263728851f4d2ed5cd8e34faee
Ehsanul Mostafa Imon <72217632+ehsanulimon@users.noreply.github.com>
2021-11-30 17:41:50 +0600

    Add files via upload

---
+++ equation solve.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#define f(x) x*x - 4*x - 10
+
+int main() {
+  int x1,x2,f;
+  cout<<"enter x1 & x2 value : "<<endl;
+  cin>>x1>>x2;
+  
+  cout<<f(x1)<<endl;
+ cout<<f(x2);
+  
+  
+  return 0;
+}
